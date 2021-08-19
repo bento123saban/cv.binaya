34d045415cb254fd26a5b85f377532d7a006526b
Bendhard16 <82221414+bento123saban@users.noreply.github.com>
2021-08-19 09:14:52 +0900

    Create order.js

---
+++ js/order.js
@@ -0,0 +1,374 @@
+const data = [
+    {
+        ID : 1,
+        nama : "Item 1",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (1).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 2,
+        nama : "Item 2",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (2).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 3,
+        nama : "Item 3",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (3).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 4,
+        nama : "Item 4",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (4).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 5,
+        nama : "Item 5",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (5).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 6,
+        nama : "Item 6",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (6).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 7,
+        nama : "Item 7",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (7).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 8,
+        nama : "Item 8",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (8).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 9,
+        nama : "Item 9",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (9).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 10,
+        nama : "Item 10",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (10).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 11,
+        nama : "Item 11",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (11).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 12,
+        nama : "Item 12",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (12).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 13,
+        nama : "Item 13",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (13).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 14,
+        nama : "Item 14",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (14).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 15,
+        nama : "Item 15",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (15).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 16,
+        nama : "Item 16",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (16).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 17,
+        nama : "Item 17",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (17).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 18,
+        nama : "Item 18",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (18).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 19,
+        nama : "Item 19",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (19).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    },
+    {
+        ID : 20,
+        nama : "Item 20",
+        deskripsi : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio omnis quibusdam eligendi quidem quae delectus cum maxime rerum ipsa minima.",
+        img : 'cat (20).jpg',
+        harga : 100000,
+        kategori : 'interior',
+        merk: ''
+    }
+]
+
+
+function tampilData(array) {
+    let html = ''
+    array.forEach(item => {
+        html += `
+        <div class="card" data-nama="${item.nama}" data-kategori="${item.kategori}" data-merk="${item.merk}">
+                <div class="card-head">
+                    <span class="tambah" data-id="${item.ID}">Order</span>
+                    <span>Rp. ${item.harga}</span>
+                </div>
+                <div class="card-konten">
+                    <div class="card-img">
+                        <img src="asset/produk/${item.img}" alt="">
+                    </div>
+                    <div class="card-desc">
+                        <p>${item.deskripsi}</p>
+                    </div>
+                </div>
+                <h5>${item.nama}</h5>
+            </div>
+        `
+    })
+    return html
+}
+
+document.querySelector('.produk-box').innerHTML = tampilData(data)
+
+
+
+function daftarOrder() // menyusun daftar item yang telah diorder
+{
+    let daftar = '' // menampung hasil dari daftar order
+    if (orderArray.length == 0 ) // jika orderArray kosong, panjang = 0, maka
+    { 
+        daftar = 
+        `
+            <p class="no-item">Belum ada item yang diorder</p> 
+        ` // daftar order kosong
+    }
+    else// tapi, jika daftar order tidak kosong, maka
+    { 
+        orderArray.forEach(function(item){
+            let html = `
+                <div class="order-item" data-id="${item.ID}">
+                    <li>${item.nama}</li>
+                    <input type="number" class="jumlah-item order-input" value="${item.jumlah}" required>
+                    <i class="fas fa-trash"></i>
+                </div>
+            `
+            daftar += html
+        })
+    }
+    return document.querySelector('.order-pilih').innerHTML = daftar // mengembalikan hasil dari daftar order menjadi HTML dari kelas order-pilih
+}
+
+function hitungTotal() // menghitung total bayar
+{
+    console.log('hitung')
+    let total = 0 // varibal menampung hasil hitung total bayar
+    orderArray.map(item => { // perulangan untuk setiap item dalam daftar order
+        total += parseFloat(item.harga) * parseFloat(item.jumlah) // mengalikan harga dari item 
+    })
+    document.querySelector('.total-harga').innerHTML = 'Total Harga : Rp. ' + total // mengembalikan/menampilkan hasil hitung pada element total harga
+}
+
+// mendapatkan data dari item dalam array, dengan parameter id sebagai id yang akan dicari dan parameter array sebagai array tempat mengambil data
+
+
+function tambahOrder(element) {
+    let ID = element.getAttribute('data-id') //mengambil ID item
+    let parameter = 0 //parameter ; jika 0 beluk ditambahkan, jika 1 sudah ditambahkan
+    orderArray.forEach( index => { //perulangan untuk mengecek item sudah ditambahkan atau belum
+        if (index.ID == ID) {
+            // index.jumlah = parseFloat(index.jumlah) + 1
+            parameter = 1 // jika sudah ditambahakan parameter menjadi (1)
+            return ''// hentikan proses
+        }
+    }) //jika item belum ditambahkan (param = 0), ambil data item dara variabel simpan data (buketData atau ritelData)
+    if(parameter == 0) //jika item belum ditambahkan (param = 0), ambil data item dara variabel simpan data (buketData atau ritelData)
+    {
+        const i = data.findIndex( item => item.ID == ID)
+        let  item = data[i] // variabel Objek untuk menampung data dari item yang ditambhakan
+        item.jumlah = 1;
+        orderArray.push(item)
+    }
+    tambahSukses()
+}
+
+var suksesInterval = ''
+function tambahSukses() {
+    let hitung = 6
+    const suksesTambah = document.querySelector("#tambah-sukses")
+
+    if (suksesTambah.classList.contains('add')) {
+        clearInterval(suksesInterval)
+        suksesTambah.classList.remove('add')
+        suksesTambah.classList.add('add')
+        suksesInterval = setInterval(() => {
+            hit()
+        }, 1000);
+    } else {
+        suksesTambah.classList.add('add')
+        suksesInterval = setInterval(() => {
+            hit()
+        }, 1000);
+    }
+    
+    function hit() {
+        hitung--
+        if (hitung == 0) {
+            clearInterval(suksesInterval)
+            suksesTambah.classList.remove('add')
+        }
+        console.log('hitung', hitung)
+    }
+}
+
+
+
+function hapusItem(element){
+    const ID = element.parentElement.getAttribute('data-id')
+    let i = orderArray.findIndex( item => item.ID == ID)
+    orderArray.splice(i, 1)
+}
+
+
+function gantiNilai(element){
+    const ID = element.parentElement.getAttribute('data-id')
+    let i = orderArray.findIndex(index => index.ID == ID)
+    let value = parseFloat(element.value)
+    if (value > 0) {
+        orderArray[i].jumlah = value
+    } else {
+        element.value = ''
+        orderArray[i].jumlah = 0
+    }
+    hitungTotal()
+}
+
+function jikaKosong(element) {
+    if (element.value == '') {
+        element.style.borderColor = 'red'
+    } else {
+        element.style.borderColor = ''
+    }
+}
+
+function cekIsi() {
+    let parameter = 0
+    document.querySelectorAll('.order-input').forEach(function (item) {
+        if (item.value == '') {
+            parameter = 1
+            item.style.borderColor = 'red'
+        }
+    })
+    if (orderArray.length == 0) {
+        parameter = 1
+    }
+    if(parameter == 0){
+        let konfirmasi = confirm('Anda akan dialihkan ke WhatsApp. Segera kirim text pesan yang tertera untuk mengkonfirmasi orderan anda !')
+        if (konfirmasi) {
+            order()
+        }
+    }
+}
+
+function order(){
+    const nama = document.querySelector('#order-nama').value
+    const telp = document.querySelector('#order-telpon').value
+    const deskripsi = document.querySelector('#order-deskripsi').value
+    const alamat = document.querySelector('#alamat').value
+    
+    let orderan = ''
+    orderArray.forEach(function(item){
+        orderan += `${item.nama} (${item.jumlah} Item).\n`
+    })
+    let total = 0 // varibal menampung hasil hitung total bayar
+    orderArray.map(item => { // perulangan untuk setiap item dalam daftar order
+        total += parseFloat(item.harga) * parseFloat(item.jumlah) // mengalikan harga dari item 
+    });
+
+    let dataWA = `Order Baru !! - ${new Date()}\n\n${nama} - ${telp}\n\n${orderan}\nTotal : Rp. ${total}\n\nDeskripsi : ${deskripsi}\n\nAlamat :\n${alamat}
+    `
+    const encode = encodeURI(dataWA)
+    const linkWA = `https://wa.me/+6282399078306?text=${encode}`
+    console.info(linkWA)
+    console.info(dataWA)
+    location.href = linkWA
+}
+
+
